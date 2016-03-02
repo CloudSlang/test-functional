2f13bdd248786fc9e0b66435a33cd262534a72c1
Bonczidai Levente <levente.bonczidai@hpe.com>
2016-03-02 11:54:59 +0200

    test case - inputs folder - 1
    Signed-off-by: Bonczidai Levente <levente.bonczidai@hpe.com>

---
+++ test_cases/cli/inputs_folder/scripts/inputs_folder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Inputs to this script:
+#   - INPUTS_TARGET_DIR <path>/inputs
+#   - INPUTS_SRC_DIR <path>/inputs
+#   - CLI_EXECUTABLE
+#   - TEST_FLOW
+
+echo "Test case started: inputs_folder"
+
+# copy input files
+echo "Copying input files ${INPUTS_SRC_DIR} -> ${INPUTS_TARGET_DIR}"
+mkdir -p ${INPUTS_TARGET_DIR}
+cp -avr ${INPUTS_TARGET_DIR} ${INPUTS_SRC_DIR}
+# run flow
+echo "Running test flow"
+${CLI_EXECUTABLE} run --f ${TEST_FLOW}
+# delete input files
+echo "Removing inputs folder: ${INPUTS_TARGET_DIR}"
+rm -r ${INPUTS_TARGET_DIR}
+
+echo "Test case finished: inputs_folder"
